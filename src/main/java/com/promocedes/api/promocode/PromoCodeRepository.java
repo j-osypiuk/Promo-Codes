121cc9c546e4e2bf1e86cac70816d24bc5385cb8
j-osypiuk <kuba6535@gmail.com>
2024-05-12 16:13:31 +0200

    Create promo code repository

---
+++ src/main/java/com/promocedes/api/promocode/PromoCodeRepository.java
@@ -0,0 +1,8 @@
+package com.promocedes.api.promocode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromoCodeRepository extends JpaRepository<PromoCode, String> {
+}
