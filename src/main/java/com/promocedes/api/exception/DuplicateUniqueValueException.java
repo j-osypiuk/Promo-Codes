f7dccc0af0dfbcab3b786289f6b71432ee6d3698
j-osypiuk <kuba6535@gmail.com>
2024-05-12 16:17:26 +0200

    Create DuplicateUniqueValueException and ObjectNotFoundException custom exceptions

---
+++ src/main/java/com/promocedes/api/exception/DuplicateUniqueValueException.java
@@ -0,0 +1,8 @@
+package com.promocedes.api.exception;
+
+public class DuplicateUniqueValueException extends RuntimeException{
+
+    public DuplicateUniqueValueException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/promocedes/api/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.promocedes.api.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
